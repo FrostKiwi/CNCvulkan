8c927aa2cf351ba9d2de53e969fa84006fbdb406
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2026-02-24 17:01:24 +0900

    Remove Submodule in preperation of CPM switch

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # volumeVulkan
 - Inital setup with MSYS2 clang64
 - Required: `cmake` `ninja`, but cmake needs to be in Windows as to get correct Backends
+# Build
+- `mkdir` a `build` folder
+- `cmake .. -G "Ninja" -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D BUILD_SHARED_LIBS=OFF`
+- Run Ninja
 # Full ban on AI
 This repo is for me to learn. No character shall touch an LLM.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <SDL3/SDL.h>
 
 int main(){
-	SDL_Log("%s", "Hello World!");
+	SDL_Window *window;
+	bool done = false;
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_CreateWindow("CNC Vulkan", 800, 600, 0);
+
+	if (window == NULL) {
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+		return 1;		
+	}
+
+	while (!done) {
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				done = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
     return 0;
 }
