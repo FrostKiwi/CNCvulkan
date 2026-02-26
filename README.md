49dd350a13266443b4fd1e2ccc61b2efcdc9212c
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2026-02-26 15:38:58 +0900

    Vulkan RAII Headers, Lib Load and Version Output

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 - Required: `cmake` `ninja`, but cmake needs to be in Windows as to get correct Backends
 # Build
 - `mkdir` a `build` folder
-- `cmake .. -G "Ninja" -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D BUILD_SHARED_LIBS=OFF`
+- `cmake .. -G "Ninja" -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D BUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug`
 - Run Ninja
 # Full ban on AI
 This repo is for me to learn. No character shall touch an LLM.
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,50 @@
+#define VULKAN_HPP_ENABLE_DYNAMIC_LOADER_TOOL 0
+#include <optional>
+#include <print>
+#include <vulkan/vulkan_core.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+#include <memory>
+#include <vulkan/vulkan_raii.hpp>
 
-struct {
-	SDL_Window *window;
-} App;
+class App {
+	std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window{nullptr, SDL_DestroyWindow};
+	bool done{false};
+	std::optional<vk::raii::Context> context{};
 
-int main(){
-	bool done = false;
+  public:
+	App() {
+		SDL_Init(SDL_INIT_VIDEO);
+		SDL_Vulkan_LoadLibrary(nullptr);
+		window.reset(SDL_CreateWindow("CNC Vulkan", 800, 600, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE));
+		auto vkGetInstanceProcAddr{reinterpret_cast<PFN_vkGetInstanceProcAddr>(SDL_Vulkan_GetVkGetInstanceProcAddr())};
+		context.emplace(vkGetInstanceProcAddr);
 
-	SDL_Init(SDL_INIT_VIDEO);
-
-	App.window = SDL_CreateWindow("CNC Vulkan", 800, 600, 0);
-
-	if (App.window == NULL) {
-		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create App.window: %s\n", SDL_GetError());
-		return 1;		
+		auto vulkanVersion{context->enumerateInstanceVersion()};
+		std::println("Vulkan Version {}.{}", VK_API_VERSION_MAJOR(vulkanVersion), VK_API_VERSION_MINOR(vulkanVersion));
 	}
 
-	while (!done) {
-		SDL_Event event;
+	~App() {
+		SDL_Quit();
+	}
 
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_EVENT_QUIT) {
-				done = true;
+	void Init() {
+	}
+
+	void Run() {
+		while (!done) {
+			for (SDL_Event event; SDL_PollEvent(&event);) {
+				if (event.type == SDL_EVENT_QUIT)
+					done = true;
 			}
 		}
 	}
+};
 
-	SDL_DestroyWindow(App.window);
+int main() {
+	App app{};
+	app.Init();
+	app.Run();
 
-	SDL_Quit();
-    return 0;
+	return 0;
 }
