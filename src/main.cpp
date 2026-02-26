3de7d529043aa58fde254f4f4b8823e4e09f1998
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2026-02-26 17:55:48 +0900

    Vulkan Instance and Surface

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #define VULKAN_HPP_ENABLE_DYNAMIC_LOADER_TOOL 0
 #include <optional>
 #include <print>
-#include <vulkan/vulkan_core.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
 #include <memory>
@@ -11,6 +10,8 @@ class App {
 	std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window{nullptr, SDL_DestroyWindow};
 	bool done{false};
 	std::optional<vk::raii::Context> context{};
+	std::optional<vk::raii::Instance> instance{};
+	std::optional<vk::raii::SurfaceKHR> surface{};
 
   public:
 	App() {
@@ -20,15 +21,27 @@ class App {
 		auto vkGetInstanceProcAddr{reinterpret_cast<PFN_vkGetInstanceProcAddr>(SDL_Vulkan_GetVkGetInstanceProcAddr())};
 		context.emplace(vkGetInstanceProcAddr);
 
-		auto vulkanVersion{context->enumerateInstanceVersion()};
-		std::println("Vulkan Version {}.{}", VK_API_VERSION_MAJOR(vulkanVersion), VK_API_VERSION_MINOR(vulkanVersion));
+		auto const vulkanVersion{context->enumerateInstanceVersion()};
+		std::println("Vulkan {}.{}", VK_API_VERSION_MAJOR(vulkanVersion), VK_API_VERSION_MINOR(vulkanVersion));
 	}
 
 	~App() {
-		SDL_Quit();
+		// Since the destructor runs before RAII Cleanup, we can't call SDL_Quit() here.
 	}
 
 	void Init() {
+		vk::ApplicationInfo applicationinfo{};
+		vk::InstanceCreateInfo instanceCreateInfo{};
+
+		uint32_t extensionCount;
+		instanceCreateInfo.ppEnabledExtensionNames = SDL_Vulkan_GetInstanceExtensions(&extensionCount);
+		instanceCreateInfo.enabledExtensionCount = extensionCount;
+
+		instance.emplace(*context, instanceCreateInfo);
+
+		VkSurfaceKHR raw_surface;
+		SDL_Vulkan_CreateSurface(window.get(), **instance, nullptr, &raw_surface);
+		surface.emplace(*instance, raw_surface)
 	}
 
 	void Run() {
