0d4a8c3c967dcb24947c5b93a280b88cae4aaaf6
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2026-03-24 13:12:21 +0900

    Rename main to SDL

--- src/SDL.cpp
+++ src/SDL.cpp
@@ -10,14 +10,17 @@ auto SDL_AppInit(void **appstate, int argc, char **argv) -> SDL_AppResult {
 
 	return result;
 }
+
 auto SDL_AppIterate(void *appstate) -> SDL_AppResult {
-	auto app = (App *)appstate;
+	auto app = static_cast<App *>(appstate);
 	return app->Iterate();
 }
+
 auto SDL_AppEvent(void *appstate, SDL_Event *event) -> SDL_AppResult {
 	auto app = static_cast<App *>(appstate);
 	return app->Event(event);
 }
+
 auto SDL_AppQuit(void *appstate, SDL_AppResult result) -> void {
 	std::unique_ptr<App> app{static_cast<App *>(appstate)};
 	app->Quit(result);
